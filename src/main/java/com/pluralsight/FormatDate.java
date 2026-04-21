6edc6d51cf7ff1e4062df49bec4427783e42c12b
Your Name <youremail@example.com>
2026-04-21 14:45:48 -0400

    I successfully made the "yyyy-MM-dd" and now I will work on making the Challenge which is "HH:mm on dd-MMM-yyyy".

--- src/main/java/com/pluralsight/FormatDate.java
+++ src/main/java/com/pluralsight/FormatDate.java
@@ -6,9 +6,15 @@ import java.time.format.DateTimeFormatter;
 
 public class FormatDate {
     public static void main(String[] args) {
+
         LocalDate currentDate = LocalDate.now();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
         String date = currentDate.format(formatter);
         System.out.println(date);
+
+        LocalDate currentDate2  = LocalDate.now();
+        DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String date2 = currentDate2.format(formatter2);
+        System.out.println(date2);
     }
 }
